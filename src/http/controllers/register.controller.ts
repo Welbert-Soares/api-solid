31ccd1121f5faf091cd14a645227f03a187ee11f
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2025-04-01 12:17:43 -0300

    fix: throw error instead of sending 500 response in registration controller

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -28,7 +28,7 @@ const register = async (request: FastifyRequest, reply: FastifyReply) => {
       return reply.status(409).send({ message: error.message })
     }
 
-    return reply.status(500).send()
+    throw error
   }
 
   return reply.status(201).send()
